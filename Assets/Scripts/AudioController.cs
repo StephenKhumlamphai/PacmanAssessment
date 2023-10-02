fc60cdfe3aa05971e49ac9e7fc163e2d85761896
StephenKhumlamphai <stephen.khumlamphai-1@student.uts.edu.au>
2023-10-03 00:05:28 +1100

    Added audio clips and script

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioClip audioClip1;
+    public AudioClip audioClip2;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayAudioClip(audioClip1);
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            // Switch to the other audio clip when the current one finishes
+            if (audioSource.clip == audioClip1)
+                PlayAudioClip(audioClip2);
+            else
+                PlayAudioClip(audioClip1);
+        }
+    }
+
+    private void PlayAudioClip(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
